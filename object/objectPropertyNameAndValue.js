0b4a9c26a2eb53f5781bd9532017cecee1003938
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2022-11-01 21:28:30 +0700

    "Add object property names & value"

---
+++ object/objectPropertyNameAndValue.js
@@ -0,0 +1,9 @@
+const person = {
+    firstName : "Eko",
+    lastName : "Kurniawan"
+};
+
+// Object.value() => Untuk mengambil semua property value
+console.log(Object.values(person));
+// Object.getOwnPropertyNames() => Untuk mengambil semua property name
+console.log(Object.getOwnPropertyNames(person));
